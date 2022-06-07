03f507639cef0310f363b2b32730f8e36899b574
yashgoyal24 <44055950+yashgoyal24@users.noreply.github.com>
2022-06-08 00:12:41 +0530

    Time: 48 ms (10.58%), Space: 23.3 MB (41.48%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -4,14 +4,15 @@ public:
         
         int n=s.size();
         
-        for(int i=0;i<n/2;i++){
-            swap(s[i],s[n-i-1]);
+       // for(int i=0;i<n/2;i++){
+          //  swap(s[i],s[n-i-1]);
           //  int initial=s[i];
           //  s[i]=s[n-i-1];
           //  s[n-i-1]=initial;
             
             
-        }
+       // }
+        reverse(s.begin(),s.end());
         
         for(int i=0;i<n;i++){
             cout<<s[i];
