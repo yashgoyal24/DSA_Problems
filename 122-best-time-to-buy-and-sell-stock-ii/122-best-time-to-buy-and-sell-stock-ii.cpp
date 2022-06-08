a9322d4f5ab36d4075e07b78f9aa85be28609c96
yashgoyal24 <44055950+yashgoyal24@users.noreply.github.com>
2022-06-08 17:29:57 +0530

    Time: 7 ms (78.49%), Space: 13.3 MB (18.14%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        return maxHandler(prices);
+        
+    }
+    
+    int maxHandler(vector<int> &prices){
+        
+        int n=prices.size();
+        int dp[n+1][2]; 
+
+        // base case
+        
+        dp[n][0]=0;dp[n][1]=0;
+        
+        for(int index=n-1;index>=0;index--){
+            
+            for(int buy=0;buy<2;buy++){
+                
+                if(buy==0){
+                    
+                    // no buy, only sell
+                    
+                    dp[index][buy] = max(prices[index]+dp[index+1][1], dp[index+1][0]);
+                    
+                    
+                }
+                else{
+                    
+                    // can buy stock
+                    
+                    dp[index][buy]=max(-prices[index]+dp[index+1][0], dp[index+1][1]);
+                    
+                }
+                
+            }
+            
+            
+        }
+        
+        return dp[0][1];
+    }
+};
