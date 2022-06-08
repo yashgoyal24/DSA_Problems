ab1ed4a8dc79c298f3ac8a7c5554f05c8ceaeef0
yashgoyal24 <44055950+yashgoyal24@users.noreply.github.com>
2022-06-08 22:58:58 +0530

    Time: 439 ms (28.68%), Space: 10.7 MB (16.07%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,11 +4,77 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         
         int n=nums.size();
-        return LIS(nums, n, 0, -1);
+        return LIS(nums, n, 0);
         
     }
     
-   /* int LIS(vector<int> &nums,int n, int curr, int prev){
+    int LIS(vector<int>& nums,int n, int index){  // result vector store the LIS elements
+        
+        vector<int> result;
+        vector<int> mapper(n,1); // to store LIS(i) at respective index i
+        vector<int> map_parent(n); // to store the parent index
+       
+        for(int i=0;i<n;i++){
+            map_parent[i]=i;
+        }
+        
+        for(int index=1;index<n;index++){
+            
+            for(int prev=0;prev<index;prev++){
+                
+                if(nums[index]>nums[prev]){
+                    
+                    if(1+mapper[prev]>mapper[index]){
+                        // set new parent
+                        map_parent[index]=prev;
+                    }
+                    mapper[index]=max(mapper[index], 1+mapper[prev]);
+                }
+                
+            }
+            
+        }
+    
+        int maxi=0;
+        for(auto it:mapper)
+            maxi=max(maxi, it);
+        
+        // find index of maxi in nums
+        
+        int max_index=-1;
+        
+        for(int i=0;i<n;i++){
+            if(mapper[i]==maxi){
+                max_index=i;
+                break;
+            }
+            
+        }
+      
+        // push max_index wala element
+        
+        result.push_back(nums[max_index]);
+        
+       
+        // start pushing parents from max_index onwards
+        
+        while(result.size()!=maxi){
+            
+            result.push_back(nums[map_parent[max_index]]);
+            int temp=map_parent[max_index];
+            max_index=temp;
+         
+            
+        }
+       
+        reverse(result.begin(),result.end());
+         for(auto it:result)
+            cout<<it;
+        return maxi;
+    }
+    
+    
+   /* int LIS(vector<int> &nums,int n, int curr, int prev){   // RECURSION APPROACH
         
         if(curr==n)
             return 0;
@@ -21,7 +87,7 @@ public:
         
     }*/
     
-    int LIS(vector<int> &nums,int n, int curr, int prev){
+  /*  int LIS(vector<int> &nums,int n, int curr, int prev){  // DP APPROACH
         
         int dp[n+1][n+1];  // curr and prev
     for(int i=0;i<=n;i++){
@@ -40,5 +106,8 @@ public:
             }
         }
         return dp[0][0];
-    }
+    }*/
+    
+    
+    
 };
