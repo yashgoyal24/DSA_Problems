7403fe0c06a584b703c0613bf5bc442142ae4620
yashgoyal24 <44055950+yashgoyal24@users.noreply.github.com>
2022-07-04 01:34:05 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (25.58%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        vector<vector<int>> matrix(m,vector<int>(n,0));
+        vector<vector<int>> dp(m,vector<int>(n,-1));
         
-        // using dp on grids
         
-        int dp[m][n];
-       // dp[0][0]=1;
-       
-        int left=0,up=0;
-        for(int i=0;i<m;i++){
-            
-            
-            for(int j=0;j<n;j++){
-                
-                if(i==0 && j==0) {
-                  // first cell base case
-                dp[i][j]=1;
-                    continue;
-                    cout<<i<<j<<dp[0][0];
-                } 
-                if(j>=1)
-                    left=dp[i][j-1];
-                else
-                    left=0;
-                if(i>=1)
-                    up=dp[i-1][j];
-                else
-                    up=0;
-                dp[i][j]=left+up; // all possible ways ka sum
-                
-               // cout<<dp[0][0];
-            }
+        return F(matrix,dp,m-1,n-1);
+    }
+    
+    int F(vector<vector<int>> &matrix,vector<vector<int>>&dp,int i,int j){
+        if(i==0&&j==0){
+            return 1; // reached destination
         }
-        
-        
-        return dp[m-1][n-1];
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int left=0,up=0;
+        left=F(matrix,dp,i,j-1);        
+        up=F(matrix,dp,i-1,j);
+        return dp[i][j]=left+up;
     }
 };
