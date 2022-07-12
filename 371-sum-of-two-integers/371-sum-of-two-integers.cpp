eb8b6b3bd7b0c0a7e1529ebf03bf47240b03130e
yashgoyal24 <44055950+yashgoyal24@users.noreply.github.com>
2022-07-12 16:51:24 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (27.01%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        
+        int exor=a^b;
+        int and1=(unsigned)(a&b)<<1;
+        
+        return exor+and1;
+        
+        
+        
     }
+    
+    
+    
+
 };
