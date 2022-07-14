1b77f5f7b91244e2df386cddc46b364b63a7b638
yashgoyal24 <44055950+yashgoyal24@users.noreply.github.com>
2022-07-14 20:40:37 +0530

    Time: 26 ms (45.39%), Space: 20.6 MB (22.34%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,43 +2,30 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         
+        // find left max and right max for all elements
         
-        //find left maximum and right maximum for all the elements
+        //left max 
         
-        vector<int> maxLeft(height.size());
-        vector<int> maxRight(height.size());
-        
-        maxLeft[0]=height[0];
-        maxRight[height.size()-1]=height[height.size()-1];
-        
-        for(int i=1;i<height.size();++i){
-            maxLeft[i]=max(maxLeft[i-1],height[i]);
-        }
-        
-        for(int i=height.size()-2;i>=0;--i){
+        int n=height.size();
+        vector<int> leftMax(n);
+        vector<int> rightMax(n);
+        leftMax[0]=height[0];
+        rightMax[n-1]=height[n-1];
+        int result=0;
+        for(int i=1;i<n;i++){
             
-                        
-             maxRight[i]=max(maxRight[i+1],height[i]);
-
+            leftMax[i]=max(height[i], leftMax[i-1]);
+           
+        }
+        for(int i=n-2;i>=0;i--){
+            rightMax[i]=max(rightMax[i+1],height[i]);
         }
         
-        for(auto it:maxLeft){
-            cout<< it<<"  ";
-        }
-        cout<<"Right";
-        for(auto it:maxRight){
-            cout<< it<<"  ";
+        for(int i=0;i<n;i++){
+            result+=(min(rightMax[i], leftMax[i])-height[i]);
         }
         
         
-        int sum=0;
-        vector<int> rain(height.size());
-        for(int i=0;i<height.size();i++){
-            
-            rain[i]=min(maxLeft[i],maxRight[i])-height[i];
-            sum+=rain[i];
-        }
-        return sum;
+        return result;
     }
-    
 };
