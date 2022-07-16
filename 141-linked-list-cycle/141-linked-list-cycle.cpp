363a1e1970bd5ac9b5d0aa8cbfef08ef599e630e
yashgoyal24 <44055950+yashgoyal24@users.noreply.github.com>
2022-07-16 23:31:53 +0530

    Time: 24 ms (20.26%), Space: 8.1 MB (16.55%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,22 +9,19 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        
-        //use slow and fast pointer to detect collision
-        
         ListNode* slow=head;
         ListNode* fast=head;
-        
-        while(fast!=NULL && fast->next!=NULL){
-            
+        while(fast!=NULL and fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
             
             if(slow==fast){
-                // cycle present
                 return true;
             }
+                
         }
+        
+        
         return false;
     }
 };
